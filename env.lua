2703812b540c231dc3b347943cc25b020740a122
yihao <yihao.dev@gmail.com>
2026-05-16 13:38:26 +0800

    add chinese input

--- env.lua
+++ env.lua
@@ -7,3 +7,11 @@
 hl.env("XCURSOR_SIZE", "18")
 hl.env("HYPRCURSOR_SIZE", "18")
 hl.env("XDG_MENU_PREFIX", "arch-")
+
+-- input method framework
+hl.env("GTK_IM_MODULE", "fcitx")
+hl.env("QT_IM_MODULE", "fcitx")
+
+-- dark theme
+-- hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+-- hl.env("QT_QPA_PLATFORM", "wayland")
