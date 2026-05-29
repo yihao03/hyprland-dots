38f1bf260ddcbdbcf3fa51a1102a66ac3b00c7ee
yihao <yihao.dev@gmail.com>
2026-05-29 19:07:12 +0800

    move monitor initialization sequence to make external default

--- config/monitors.lua
+++ config/monitors.lua
@@ -2,17 +2,6 @@
 ---- MONITORS ----
 ------------------
 
--- See https://wiki.hypr.land/Configuring/Basics/Monitors/
-hl.monitor({
-	output = "eDP-1",
-	mode = "preferred",
-	position = "auto",
-	scale = "1.5",
-	-- icc = "/home/yihao/.local/share/icc/default.icm",
-	cm = "auto",
-	bitdepth = 10,
-})
-
 hl.monitor({
 	output = "desc:Acer Technologies EK221Q H 1335088483W01",
 	mode = "1920x1080@100",
@@ -24,6 +13,17 @@ hl.monitor({
 	mode = "1920x1080@120",
 	position = "auto-center-left",
 	cm = "auto",
+})
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+	output = "eDP-1",
+	mode = "preferred",
+	position = "auto",
+	scale = "1.5",
+	-- icc = "/home/yihao/.local/share/icc/default.icm",
+	cm = "auto",
+	bitdepth = 10,
 })
 
 -- com3 b1 tv
