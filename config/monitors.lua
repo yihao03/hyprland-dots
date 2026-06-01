aee9a57a9f6db78b705f245893d39c49889a989e
yihao <yihao.dev@gmail.com>
2026-06-01 19:53:18 +0800

    Consolidate display controls and update config

--- config/monitors.lua
+++ config/monitors.lua
@@ -26,10 +26,94 @@ hl.monitor({
 	bitdepth = 10,
 })
 
--- com3 b1 tv
 hl.monitor({
-	output = "desc:Samsung Electric Company SyncMaster H1AK500000",
-	position = "auto-center-up",
-	mode = "3840x2160@30",
-	scale = 3,
+	output = "",
+	mode = "preferred",
+	position = "auto",
+	scale = 1,
+	mirror = "eDP-1",
 })
+
+----------------------------
+---- DISPLAY SHORTCUTS  ----
+----------------------------
+local mainMod = require("config.keybinds").mainMod
+local lid_closed = false
+local toggle_built_in = false
+local built_in_disabled = false
+
+hl.bind("switch:on:Lid Switch", function()
+	local monitors = hl.get_monitors()
+	lid_closed = true
+	if #monitors == 1 then
+		hl.dispatch(
+			hl.dsp.exec_cmd("sh -c 'qs -c noctalia-shell ipc call lockScreen lock && sleep 1 && systemctl suspend'")
+		)
+	else
+		hl.monitor({ output = "eDP-1", disabled = true })
+	end
+end, { locked = true })
+
+hl.bind("switch:off:Lid Switch", function()
+	lid_closed = false
+	hl.monitor({ output = "eDP-1", disabled = false })
+	hl.exec_cmd("hyprctl reload")
+end, { locked = true })
+
+hl.bind(mainMod .. " + P", hl.dsp.submap("displayControl"))
+
+hl.define_submap("displayControl", "reset", function()
+	hl.bind("D", function()
+		local monitors = hl.get_monitors()
+		if #monitors == 1 and monitors[1].name == "eDP-1" then
+			hl.notification.create({ text = "No external monitor detected", duration = 3000 })
+			return
+		end
+
+		if lid_closed then
+			return
+		end
+		built_in_disabled = not built_in_disabled
+		if built_in_disabled then
+			toggle_built_in = true
+		end
+		hl.monitor({ output = "eDP-1", disabled = built_in_disabled })
+	end)
+
+	hl.bind("S", function()
+		local monitors = hl.get_monitors()
+		if #monitors == 1 then
+			hl.notification.create({ text = "No external monitor detected", duration = 3000 })
+			return
+		end
+
+		for _, monitor in ipairs(monitors) do
+			if monitor.name ~= "eDP-1" then
+				hl.exec_cmd("wl-mirror eDP-1", { monitor = monitor.name, fullscreen = true })
+				return
+			end
+		end
+	end)
+
+	hl.bind("H", function()
+		local window = hl.get_window("class:at.yrlf.wl_mirror")
+		if window then
+			hl.dispatch(hl.dsp.dpms({ monitor = window.monitor.name, action = "toggle" }))
+		else
+			hl.notification.create({ text = "No mirrored monitor found", duration = 3000 })
+		end
+	end)
+end)
+
+hl.on("monitor.removed", function()
+	if toggle_built_in then
+		toggle_built_in = false
+		return
+	end
+
+	if not lid_closed then
+		built_in_disabled = false
+		hl.monitor({ output = "eDP-1", disabled = built_in_disabled })
+		hl.exec_cmd("hyprctl reload")
+	end
+end)
