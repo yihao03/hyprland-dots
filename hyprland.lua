c08274276180237834a4330f6c126eacd3f681f0
yihao <yihao.dev@gmail.com>
2026-06-02 08:20:02 +0800

    cleanup

--- hyprland.lua
+++ hyprland.lua
@@ -1,15 +1,3 @@
--- Disabled after converting the configuration back to Hyprlang in hyprland.conf.
--- This was the Lua entrypoint; the split Lua files remain as a backup/reference.
--- Refer to the wiki for more information.
--- https://wiki.hypr.land/Configuring/Start/
-
--- Please note not all available settings / options are set here.
--- For a full list, see the wiki
-
--- You can (and should!!) split this configuration into multiple files
--- Create your files separately and then require them like this:
--- require("myColors")
-
 require("config.monitors")
 require("config.autostart")
 require("config.env")
