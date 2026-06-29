f1e343cfd19489e3f119d1f2dba598e01062ecd6
yihao <yihao.dev@gmail.com>
2026-06-29 08:07:31 +0800

    Use noctalia for sunset

--- config/autostart.lua
+++ config/autostart.lua
@@ -2,23 +2,6 @@
 
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
-
-local function set_sunset()
-	hl.exec_cmd("hyprsunset")
-	-- Set hyprsunset based on time (matches hyprsunset.conf profiles)
-	local hour = tonumber(os.date("%H"))
-	local minute = tonumber(os.date("%M"))
-	local time_minutes = hour * 60 + minute
-	local sunrise = 7 * 60 + 30 -- 7:30
-	local sunset = 18 * 60 -- 18:00
-
-	if time_minutes >= sunrise and time_minutes < sunset then
-		hl.exec_cmd("hyprctl hyprsunset identity")
-	else
-		hl.exec_cmd("hyprctl hyprsunset temperature 4000")
-	end
-end
-
 hl.on("hyprland.start", function()
 	-- important stuff
 	hl.exec_cmd("noctalia")
@@ -31,5 +14,4 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("thunderbird", { workspace = "special:magic" })
 
 	hl.exec_cmd("hyprpm reload")
-	set_sunset()
 end)
